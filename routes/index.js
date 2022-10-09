3229f4a6e03300487ad48edd24fc7c11f32a3abb
ryan345676 <tsangtszlong@gmail.com>
2022-10-09 13:50:13 -0400

    refactor,add js function, final edit

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'home' });
 });
-/* GET home page. */
+
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'about' });
 });
-/* GET home page. */
+
 router.get('/project', function(req, res, next) {
   res.render('project', { title: 'project' });
 });
-/* GET home page. */
+
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'services' });
 });
-/* GET home page. */
+
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'contact' });
 });
